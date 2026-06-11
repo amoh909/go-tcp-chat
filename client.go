f09645f0121c223bc6d88dde91f6cd03348e329e
Ali <ali2moh.04@gmail.com>
2026-06-11 22:49:22 +0300

    Implemented client and server

---
+++ client.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "net"
+    "os"
+)
+
+func main() {
+    conn, err := net.Dial("tcp", "localhost:8080")
+    if err != nil {
+        fmt.Println("Failed to connect to server:", err)
+        return
+    }
+    defer conn.Close()
+
+    reader := bufio.NewReader(os.Stdin)
+    for {
+        fmt.Print("Enter message: ")
+        message, _ := reader.ReadString('\n')
+        // Send the message to the server
+        _, err := conn.Write([]byte(message))
+        if err != nil {
+            fmt.Println("Failed to send message:", err)
+            return
+        }
+        response, err := bufio.NewReader(conn).ReadString('\n')
+        if err != nil {
+            fmt.Println("Failed to receive message:", err)
+            return
+        }
+        fmt.Print("Server echo: " + response)
+    }
+}
